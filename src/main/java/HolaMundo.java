3434e38175619963f77a0383d2d1be82a1395b7b
E2000\buceta <davidbuceta@ebroker.es>
2017-12-29 14:32:10 +0100

    inital commit

---
+++ src/main/java/HolaMundo.java
@@ -0,0 +1,16 @@
+
+
+
+
+public class HolaMundo {
+    static void hola(){
+        System.out.println("Hola Jenkins y los demas!OB");
+    
+    }
+
+    public static void main (String[] args){
+        hola();
+    }
+
+
+}
---
+++ src/test/java/HolaMundoTest.java
@@ -0,0 +1,18 @@
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class HolaMundoTest {
+
+    @Test
+    public void testHola() {
+    	HolaMundo test = new HolaMundo();
+    	test.hola();
+    	assertTrue(true);
+    }
+}
+
+
